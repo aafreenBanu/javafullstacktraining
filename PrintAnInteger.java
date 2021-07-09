5b1dd79d970ce8bcd08711a1513740ec35ace651
aafreenBanu <80515380+aafreenBanu@users.noreply.github.com>
2021-07-09 17:12:40 +0530

    Add files via upload

---
+++ PrintAnInteger.java
@@ -0,0 +1,13 @@
+package knightingale;
+import java.util.Scanner;
+public class PrintAnInteger {
+	public static void main(String[] args) {
+		Scanner reader=new Scanner(System.in);
+		System.out.println("enter a number:");
+		int number=reader.nextInt();
+		System.out.println("you entered:"+number);
+		
+		
+	}
+
+}
