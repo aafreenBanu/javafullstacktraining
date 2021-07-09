7694569473bd105b66fa195706f1c6d4615cca2d
aafreenBanu <80515380+aafreenBanu@users.noreply.github.com>
2021-07-09 17:13:51 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,17 @@
+package knightingale;
+import java.util.Scanner;
+public class Multiply {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter a value for i:");
+		int i=scan.nextInt();
+		System.out.println("enter a value of j:");
+		int j=scan.nextInt();
+		int mul=i*j;
+		System.out.println("the multiplied value is:" +mul);
+		
+
+	}
+
+}
