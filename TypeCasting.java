548cd319ea1f38b16c2d7dc6198cff61b2d80f41
aafreenBanu <80515380+aafreenBanu@users.noreply.github.com>
2021-07-09 17:14:04 +0530

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,16 @@
+package knightingale;
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		byte b=10;
+		int i=b;
+		byte c=i;
+		byte d=(byte)i;
+		byte a=1;
+		byte n=2;
+		byte sum=a*n;
+		
+		
+	}
+
+}
