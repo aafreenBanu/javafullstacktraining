537b95c06c41e44c866f2e39153da7c68575e085
aafreenBanu <80515380+aafreenBanu@users.noreply.github.com>
2021-07-09 17:14:57 +0530

    Add files via upload

---
+++ VowelOrNot.java
@@ -0,0 +1,12 @@
+package knightingale;
+
+public class VowelOrNot {
+	public static void main(String[] args) {
+		char ch='z';
+		 if(ch =='a' || ch == 'e' || ch =='i' || ch =='o' || ch== 'u')
+		   System.out.println(ch + " its an a vowel");
+		 else
+		   System.out.println(ch + " its not a vowel");
+	}
+
+}
