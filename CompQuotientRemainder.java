70d166db90362bba0362e08170b30936775d118e
aafreenBanu <80515380+aafreenBanu@users.noreply.github.com>
2021-07-09 17:14:49 +0530

    Add files via upload

---
+++ CompQuotientRemainder.java
@@ -0,0 +1,15 @@
+package knightingale;
+
+public class CompQuotientRemainder {
+	public static void main(String[] args) {
+		int dividend=25;
+		int divisor=4;
+		int quotient=dividend/divisor;
+		int remainder=dividend%divisor;
+		System.out.println("quotient is : " + quotient);
+		System.out.println("remainder is :" + remainder);
+	}
+}
+
+
+
