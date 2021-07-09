27e77598ff5d14fd9be80936d469108648ec98f9
aafreenBanu <80515380+aafreenBanu@users.noreply.github.com>
2021-07-09 17:14:13 +0530

    Add files via upload

---
+++ AsciiValue.java
@@ -0,0 +1,13 @@
+package knightingale;
+
+public interface AsciiValue {
+	public static void main(String[] args) {
+		char ch='A';
+		int ascii=ch;
+		int castascii=(int)ch;
+		System.out.println("the ascii value of " + ch + " is :" +ascii);
+		System.out.println("the ascii value of " + ch + " is :" +castascii);
+		
+	}
+
+}
